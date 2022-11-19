9af57b2b14c22d5255250f552e6b1635675c4b8d
skotovrach <skotovrach@yandex.ru>
2022-11-19 03:09:13 +0300

    added js

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector('.price-img');
+document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.3) / 8) + 'px,' + ((event.clientY * 0.3) / 8) + 'px,0px)';
+});
+
+    const elem = document.querySelector(".main");
+document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' + (0.3 * window.pageYOffset) + 'px';
+})
+});
