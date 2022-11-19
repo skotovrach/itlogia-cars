24580361795ed6731fb8175bd6895cfd5f172407
skotovrach <skotovrach@yandex.ru>
2022-11-19 16:44:48 +0300

    added scroll to main button

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
+document.getElementById('main-action').onClick = function() {
+    document.getElementById('cars').scrollIntoView(true)
+};
+
 document.addEventListener("DOMContentLoaded", function () {
     let layer = document.querySelector('.price-img');
 document.addEventListener('mousemove', (event) => {
-        layer.style.transform = 'translate3d(' + ((event.clientX * 0.3) / 8) + 'px,' + ((event.clientY * 0.3) / 8) + 'px,0px)';
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.1) / 1) + 'px,' + ((event.clientY * 0.2) / 5) + 'px,0px)';
 });
 
     const elem = document.querySelector(".main");
 document.addEventListener('scroll', () => {
-        elem.style.backgroundPositionX = '0' + (0.3 * window.pageYOffset) + 'px';
+        elem.style.backgroundPositionX = '0' + (0.2 * window.pageYOffset) + 'px';
 })
 });
